26ae550a933b716aa36bc83088434680a1e3ebd3
Oni Noor <rezpector335@gmail.com>
2022-10-17 13:35:25 +0700

    Membuat section partner

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Categories from "components/Categories";
 import Destinations from "components/Destinations";
 import EasyAndFast from "components/EasyAndFast";
 import Testimonials from "components/Testimonials";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -42,6 +43,64 @@ export default function Home() {
 
       {/* Testimonials */}
       <Testimonials />
+
+      {/* Partner */}
+      <section className="mb-28">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex">
+            <div className="w-1/5 px-4">
+              <div className="h-20 px-4 py-4 relative grayscale hover:grayscale-0 bg-white transform hover:-translate-y-1 hover:shadow-great rounded-2xl transition-all duration-300">
+                <Image
+                  className="object-none"
+                  layout="fill"
+                  src={"/images/axon.png"}
+                  alt="in Partnership with Axon"
+                />
+              </div>
+            </div>
+            <div className="w-1/5 px-4">
+              <div className="h-20 px-4 py-4 relative grayscale hover:grayscale-0 bg-white transform hover:-translate-y-1 hover:shadow-great rounded-2xl transition-all duration-300">
+                <Image
+                  className="object-none"
+                  layout="fill"
+                  src={"/images/jetstar.png"}
+                  alt="in Partnership with Jetstar"
+                />
+              </div>
+            </div>
+            <div className="w-1/5 px-4">
+              <div className="h-20 px-4 py-4 relative grayscale hover:grayscale-0 bg-white transform hover:-translate-y-1 hover:shadow-great rounded-2xl transition-all duration-300">
+                <Image
+                  className="object-none"
+                  layout="fill"
+                  src={"/images/expedia.png"}
+                  alt="in Partnership with Expedia"
+                />
+              </div>
+            </div>
+            <div className="w-1/5 px-4">
+              <div className="h-20 px-4 py-4 relative grayscale hover:grayscale-0 bg-white transform hover:-translate-y-1 hover:shadow-great rounded-2xl transition-all duration-300">
+                <Image
+                  className="object-none"
+                  layout="fill"
+                  src={"/images/qantas.png"}
+                  alt="in Partnership with Qantas"
+                />
+              </div>
+            </div>
+            <div className="w-1/5 px-4">
+              <div className="h-20 px-4 py-4 relative grayscale hover:grayscale-0 bg-white transform hover:-translate-y-1 hover:shadow-great rounded-2xl transition-all duration-300">
+                <Image
+                  className="object-none"
+                  layout="fill"
+                  src={"/images/alitalia.png"}
+                  alt="in Partnership with Alitalia"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
